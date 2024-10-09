8589db53657bd93ff96a95241d9b98a9b93b773e
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-09 21:01:38 +0200

    Fixes

--- MKL2Syldeyn/Program.cs
+++ MKL2Syldeyn/Program.cs
@@ -1,4 +1,30 @@
-﻿
+// License:
+// 
+// Syldeyn
+// Convert old MKL license files to Syldeyn format
+// 
+// 
+// 
+// 	(c) Jeroen P. Broks, 2024
+// 
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 	Please note that some references to data like pictures or audio, do not automatically
+// 	fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 24.10.09
+// End License
+
 
 /*
  * This little utility is only meant to set the old MKL data into a format that Syldeyn can understand.
@@ -8,6 +34,6 @@
  * completely converted, I won't really need this tool anymore, do I?
  */
 
-foreach(var dir in args) {
+foreach (var dir in args) {
 	Console.WriteLine($"Processing {dir}");
 }
--- Syldeyn/Syldeyn.cpp
+++ Syldeyn/Syldeyn.cpp
@@ -1,3 +1,29 @@
+// License:
+// 
+// Syldeyn
+// License block and version updater
+// 
+// 
+// 
+// 	(c) Jeroen P. Broks, 2024
+// 
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 	Please note that some references to data like pictures or audio, do not automatically
+// 	fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 24.10.09
+// End License
 #include "Syldeyn_Config.hpp"
 
 #include <SlyvQCol.hpp>
@@ -64,7 +90,7 @@ namespace Slyvina {
 				std::cout << "Debug block #" << num << ": " << SB->Name << "; (" << SB->ID << ")\n";
 				for (auto LLL : *LL) {
 					auto NL{ StReplace(LLL,"<$tab>","\t") }; NL = StReplace(NL, "<$spc>", " ");
-					if (LLL == "$WL") B->License.push_back(""); else B->License.push_back(NL);
+					if (Upper(LLL) == "$WL") B->License.push_back(""); else B->License.push_back(NL);
 				}
 				for (auto FLD : B->Fields) {
 					B->Defaults[FLD] = BlckG->Value(C, "Default." + FLD);
@@ -294,6 +320,7 @@ namespace Slyvina {
 					for (auto l : LTW) {
 						BW->WriteLine(l);
 					}
+					BW->Close();
 				}
 			}
 #pragma endregion
